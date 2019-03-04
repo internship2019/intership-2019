3a8cae5e53bec1c6c79a0cd0c93601b4666c6000
Emil Terman <terman.emil@gmail.com>
2019-03-04 13:13:04 +0200

    Added Tests for MethodInfoDescriptor

---
+++ src/DotNetEssentials/ObjectBrowser/ObjectBrowserTests/MetadataMenu/DummyClassForTesting.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ObjectBrowserTests.MetadataMenu
+{
+    /*
+     * A class used for testing.
+    **/   
+    public class DummyClassForTesting
+    {
+        public int aVariable;
+
+        public void MethodWithNoParams()
+        {
+        }
+
+        public void MethodWithAParam(int theParam)
+        {
+        }
+
+        public void MethodWithNullDefaultParameter(string theDefaultParameter = null)
+        {
+        }
+
+        public void MethodWithDefaultParameter(int theDefaultParameter = 42)
+        {
+        }
+
+        public void MethodWithOutParameter(out int theOutParameter)
+        {
+            theOutParameter = 42;
+        }
+
+        public void MethodWithStrParamType(string theStrParam)
+        {
+        }
+    }
+}
---
+++ src/DotNetEssentials/ObjectBrowser/ObjectBrowserTests/MetadataMenu/MethodInfoDescriptorTests.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+using ObjectBrowser.MetadataMenu;
+
+namespace ObjectBrowserTests.MetadataMenu
+{
+    public class MethodInfoDescriptorTests
+    {
+        private MethodInfoDescriptor target;
+
+        [SetUp]
+        public void SetUp()
+        {
+            target = new MethodInfoDescriptor();
+        }
+
+        [Test]
+        public void Describe_NotAMethod_RetunrNull()
+        {
+            // Arrange
+            var memberInfo = typeof(DummyClassForTesting).GetMembers().First(x => !(x is MethodInfo));
+
+            // Act
+            var output = target.Describe(memberInfo);
+
+            // Assert
+            Assert.IsNull(output);
+        }
+
+        [Test]
+        public void Describe_NoParams_ShowNothing()
+        {
+            // Arrange
+            var methodName = nameof(DummyClassForTesting.MethodWithNoParams);
+            var methodInfo = typeof(DummyClassForTesting).GetMethod(methodName);
+
+            // Act
+            var output = target.Describe(methodInfo);
+
+            // Assert
+            Assert.IsFalse(output.Any());
+        }
+
+        [Test]
+        public void Describe_ShowCorrectParamName()
+        {
+            // Arrange
+            var methodName = nameof(DummyClassForTesting.MethodWithAParam);
+            var methodInfo = typeof(DummyClassForTesting).GetMethod(methodName);
+
+            // Act
+            var output = target.Describe(methodInfo);
+
+            // Assert
+            var paramName = methodInfo.GetParameters().First().Name;
+            var rx = new Regex($".*{paramName}.*");
+
+            Assert.IsTrue(LinesMentionPattern(output, rx));
+        }
+
+        [Test]
+        public void Describe_HasDefaultNullParam_ShowDefaultNull()
+        {
+            // Arrange
+            var methodName = nameof(DummyClassForTesting.MethodWithNullDefaultParameter);
+            var methodInfo = typeof(DummyClassForTesting).GetMethod(methodName);
+
+            // Act
+            var output = target.Describe(methodInfo);
+
+            // Assert
+            var rx = new Regex(@".*Default.*null.*", RegexOptions.IgnoreCase);
+            Assert.IsTrue(LinesMentionPattern(output, rx));
+        }
+
+        [Test]
+        public void Describe_HasDefaultParam_ShowDefaultValue()
+        {
+            // Arrange
+            var methodName = nameof(DummyClassForTesting.MethodWithDefaultParameter);
+            var methodInfo = typeof(DummyClassForTesting).GetMethod(methodName);
+
+            // Act
+            var output = target.Describe(methodInfo);
+
+            // Assert
+            var defaultValue = methodInfo.GetParameters().First().DefaultValue;
+            var rx = new Regex($".*Default.*{defaultValue}.*", RegexOptions.IgnoreCase);
+            Assert.IsTrue(LinesMentionPattern(output, rx));
+        }
+
+        [Test]
+        public void Describe_HasOutParameter_ShowParamHasOut()
+        {
+            // Arrange
+            var methodName = nameof(DummyClassForTesting.MethodWithOutParameter);
+            var methodInfo = typeof(DummyClassForTesting).GetMethod(methodName);
+
+            // Act
+            var output = target.Describe(methodInfo);
+
+            // Assert
+            var paramName = methodInfo.GetParameters().First(x => x.IsOut).Name;
+            var rx = new Regex($".*{paramName}.*out.*", RegexOptions.IgnoreCase);
+
+            Assert.IsTrue(LinesMentionPattern(output, rx));
+        }
+
+        [Test]
+        public void Describe_HasStringParamType_ShowCorrectParamType()
+        {
+            // Arrange
+            var methodName = nameof(DummyClassForTesting.MethodWithStrParamType);
+            var methodInfo = typeof(DummyClassForTesting).GetMethod(methodName);
+
+            // Act
+            var output = target.Describe(methodInfo);
+
+            // Assert
+            var paramName = methodInfo.GetParameters().First().Name;
+            var rx = new Regex($".*{paramName}.*string.*", RegexOptions.IgnoreCase);
+
+            Assert.IsTrue(LinesMentionPattern(output, rx));
+        }
+
+        private bool LinesMentionPattern(IEnumerable<string> lines, Regex pattern)
+        {
+            var targetLineMentioned = false;
+            foreach (var line in lines)
+            {
+                if (pattern.IsMatch(line))
+                {
+                    targetLineMentioned = true;
+                    break;
+                }
+            }
+
+            return targetLineMentioned;
+        }
+    }
+}
